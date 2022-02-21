a152078e6a5e023e18e06405cc67bbf3ecc2cfa1
zubaidr <web.expert1221@gmail.com>
2022-02-21 19:28:07 +0500

    update todo-listitem.componetes.html src\app\\todo-listitem

--- src/app/todo-listitem/todo-listitem.component.ts
+++ src/app/todo-listitem/todo-listitem.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-listitem',
@@ -8,35 +8,22 @@ import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 export class TodoListitemComponent implements OnInit {
   todo = "";
   @Input ()todos:any[]=[];
-  @Input ()selectedindex:any;
   @Output  () deletevalue =new EventEmitter();
   @Output () edittodo =new EventEmitter();
 
-  // constructor() {
-  //   this.localitem = localStorage.getItem("todolist")
-  //   if (this.localitem == null) {
-  //     this.todos = [];
-  //   } else {
-  //     this.todos = JSON.parse(this.localitem)
 
-  //   }
 
-  // }
   ngOnInit(): void {
+  
+    
   }
 
   changeValue($event: any) {
     localStorage.setItem("todolist", JSON.stringify(this.todos))
   }
+
   editbtn(item: any, index: number) {
-    this.todo = item.Name;
-    this.selectedindex = index;
-    console.warn(this.todo)
-    localStorage.setItem("todolist", JSON.stringify(this.todos))
-    // this.show = !this.show;
-    // this.showme = !this.showme;
     this.edittodo.emit({item,index})
-    // console.log(item,index)
 
   }
   delteitem(index: number) {
